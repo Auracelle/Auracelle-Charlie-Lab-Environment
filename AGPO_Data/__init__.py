f89cd159eb486a359304f9bc012a7ccbb1caf912
Grace-Alice Evans <EvansAGrace@gmail.com>
2026-03-23 14:51:47 -0400

    Add files via upload

---
+++ AGPO_Data/__init__.py
@@ -0,0 +1,9 @@
+"""
+AGPO Data Package - AI Governance Policy Optimization
+Real-world data integration for wargaming simulations
+"""
+from .worldbank import *
+from .exportcontrol import *
+from .sipri import *
+
+__version__ = "1.0.0"
